9d03a2dce7506aaf0a2751b7c8140ced9fe017f3
tont1990 <tont1990@gmail.com>
2024-05-17 13:12:51 +0900

    Init Project

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 5003,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+})
